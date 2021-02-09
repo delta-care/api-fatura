53551d5632712086d3b5af4a2702cd6608b74232
David Carvalho <jdavidsilv@gmail.com>
2021-02-09 20:23:48 -0300

    Adição cors

--- src/main/java/xyz/deltacare/fatura/configuration/CorsConfig.java
+++ src/main/java/xyz/deltacare/fatura/configuration/CorsConfig.java
@@ -1,8 +1,10 @@
 package xyz.deltacare.fatura.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
